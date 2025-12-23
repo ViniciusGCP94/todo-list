395c9e61846c47ec57e6bfec6df3fa09b517e33a
ViniciusGCP94 <vinniegcp@gmail.com>
2025-12-22 22:16:10 -0300

    feat: implementacao do componente TaskItem com suporte a exclusão e estilização Tailwind. - TaskItem adiciona a task a lista - Icone de lixeira exclui o item adicionado.

--- todo-list-v3/src/App.jsx
+++ todo-list-v3/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { useState } from 'react'
 import Header from './components/header/header'
+import TaskItem from './components/taskItem/TaskItem';
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -15,16 +16,24 @@ function App() {
         };
 
         setTasks([...tasks, newTask]);
-      }
+    }
+
+    const deleteTask = (id) =>{
+      const updatedTasks = tasks.filter(task => task.id !== id);
+      setTasks(updatedTasks)
+    }
 
   return (
     <>
       < Header onAddTask={addTask} />
       <main className="p-4 max-w-2xl mx-auto flex flex-col gap-4">
         {tasks.map((task) => (
-          <div key={task.id} className="bg-indigo-950 p-4 rounded-lg border border-gray-500 text-white">
-            {task.text}
-          </div>
+          <TaskItem
+            key={task.id}
+            task={task}
+            onDelete={deleteTask} 
+            className="bg-indigo-950 p-4 rounded-lg border border-gray-500 text-white"
+          />   
         ))}
       </main>
     </>
---
+++ todo-list-v3/src/components/taskItem/TaskItem.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { LuTrash2 } from "react-icons/lu";
+
+const TaskItem = ({task, onDelete}) => {
+    return(
+        <div className="flex items-center justify-between bg-indigo-950 p-4 rounded-xl border border-gray-500 shadow-lg mb-3">
+            <div className="flex items-center gap-3 justify-between">
+                <input type="checkbox" 
+                    className="task-checkbox w-7 h-7 appearance-none border-2 rounded-full border-gray-50 bg-gray-50 checked:bg-green-200 checked:border-green-200 relative"
+                />
+                <span className="text-gray-100 font-medium">{task.text}</span>
+            </div>
+            <button
+                onClick={() => onDelete(task.id)}
+                className="text-gray-400 hover:text-red-500 transition-colors p-2 cursor-pointer"
+                title="Excluir tarefa"
+            >
+                <LuTrash2 size={20} />
+            </button>
+            
+        </div>
+    );
+};
+
+export default TaskItem;
