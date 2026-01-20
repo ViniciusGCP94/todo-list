f47de5857d93cb6fcdd6da51f7aeb2fe145b75d0
ViniciusGCP94 <vinniegcp@gmail.com>
2026-01-19 21:43:49 -0300

    refactor: adição do componente ActionButton na filterBar, movendo paddings da base para variantes limpando a lógica de filtros. - Base sem os paddings que foram passados para as outras variants, para não haver erros na estilização dos botões do Modal e dos botões da filterBar.

--- todo-list-v3/src/components/common/ActionButton.jsx
+++ todo-list-v3/src/components/common/ActionButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const buttonVariants = {
-    base: "font-bold transition-all flex",
+    base: "font-bold transition-all flex items-center justify-center",
     neon: "hover:shadow-[0_0_20px_rgba(79,70,229,0.5)] ",
     filter: "bg-indigo-950 border-indigo-950 text-gray-300 px-2 py-0.5 border-2 rounded-full",
     filterActive: "bg-indigo-800 border-amber-50 text-white px-2 py-0.5 border-2 rounded-full",
